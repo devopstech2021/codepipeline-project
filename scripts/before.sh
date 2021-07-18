63a50df9de5d9d83abc75a65414243c8515a09c1
root <root@ip-172-26-13-30.ec2.internal>
2021-07-18 10:20:25 +0000

    test

--- scripts/before.sh
+++ scripts/before.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
-
 rm -rf /tmp/deploy
 mkdir -p /tmp/deploy
-
 if [ $? -eq 0 ]; then
-	echo "Directory created"
+echo "Directory created"
 else
-	echo "Directory not created"
+echo "Directory not created"
 fi
-
 chown -R sudo:sudo /tmp/deploy
-
 if [ $? -eq 0 ]; then
-	echo "OK"
+echo "OK"
 else
-	echo "Fail killing process"
+echo "Fail killing process"
 fi
