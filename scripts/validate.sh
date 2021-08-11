62b9b310a76174b430077a346146aae7c04ce479
devopstech2021 <85070936+devopstech2021@users.noreply.github.com>
2021-08-12 03:45:36 +0530

    Update validate.sh

--- scripts/validate.sh
+++ scripts/validate.sh
@@ -2,8 +2,7 @@
 sleep 2
 
 if [ "$DEPLOYMENT_GROUP_NAME" == " growexx-devops-deploy" ]; then
-
-result=$(curl -sL -w "%{http_code}\\n" "https://test.growexx.com/" -o /dev/null)
+result=$(curl -sL -w "%{http_code}\\n" "http://awstechnology.ml/" -o /dev/null)
 fi
 
 if [ "$result" = "200" ]; then
